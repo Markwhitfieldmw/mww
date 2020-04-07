edf4fe2e88675bde1c7756039166c13edda39ea9
Markwhitfieldmw <63299786+Markwhitfieldmw@users.noreply.github.com>
2020-04-07 14:38:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let name ="Mark"
+let age = 44
+let jobrole = "Director"
+
+console.log("Hello, I am " + name + " and I am " + age + " years old ")
+
+
+
+
+
+let numberOne = 20
+let numberTwo = 40
+
+function addition(numberOne, numberTwo){
+    let total = numberOne + numberTwo
+    console.log(total)
+}
+
+addition(50, 40)
+addition(30, 50)
+addition(300, 550)
+addition(330, 530)
+
+
